63209592801480a235a533b1f107eb21a63c32fb
DevExpressExampleBot <fakeEmail>
2021-09-01 18:17:16 +0300

    Source auto update [skip ci]

--- VB/Chart3D_Lesson1/MainWindow.xaml.vb
+++ VB/Chart3D_Lesson1/MainWindow.xaml.vb
@@ -14,14 +14,14 @@ Imports System.Windows.Navigation
 Imports System.Windows.Shapes
 
 Namespace Chart3D_Lesson1
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
 End Namespace
